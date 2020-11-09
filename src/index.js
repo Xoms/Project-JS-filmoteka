a828314fe02697f45281074c1a0f6956b77a00bf
Heorhiivv <georgevbuisness@gmail.com>
2020-11-09 17:06:34 +0200

    add modal hbs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import debounce from 'lodash.debounce';
 
 import './sass/main.scss';
 import 'basiclightbox/dist/basicLightbox.min.css';
+import './js/modal.js'
 
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,2 +1,12 @@
 import api from "./api/apiService";
-import genres from "./api/genresDb"
+import genres from "./api/genresDb";
+import movieItem from "../partials/modal.hbs";
+
+
+
+// api.getTrends()
+// .then((movies, posters) => {
+//   const movieIno = movies.result.map((el, i) => {
+
+//   })
+// }
