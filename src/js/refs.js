09f300ae2539830da9159b760bfb59db48225d2e
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-11-09 17:15:03 +0100

    fixing errors in refs from my prev commit

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export default {
   ul: document.querySelector('.movies'),
-  userInputField: document.querySelector(".input-search");
- errorMessage: document.querySelector(".error-message");
+  userInputField: document.querySelector(".input-search"),
+ errorMessage: document.querySelector(".error-message"),
 }
