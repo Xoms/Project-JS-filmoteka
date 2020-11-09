aff99662b290be1381f28dd1c05490539e66d725
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-09 19:56:11 +0200

    добавляет

--- src/js/api/library.js
+++ src/js/api/library.js
@@ -1,8 +1,12 @@
-import api from './apiService.js'
+import api from './apiService.js';
+import main from '../main.js';
 
 const btn = document.querySelector('.queue-btn')
 const goToLibrary = function () {
   const filmArr = JSON.parse(localStorage.getItem('toWatch'));
 
 }
+console.log(JSON.parse(localStorage.getItem('toWatch')));
 console.log(api.getTrends());
+
+// btn, orange, white
