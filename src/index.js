31cda5fa83c5b47a4801b850705e2af22c3f3a3e
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-09 17:05:28 +0200

    вносит изменения

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import debounce from 'lodash.debounce';
 
 import './sass/main.scss';
 import 'basiclightbox/dist/basicLightbox.min.css';
-
+import './js/api/library.js'
--- src/js/api/library.js
+++ src/js/api/library.js
@@ -1,5 +1,8 @@
+import api from './apiService.js'
+
 const btn = document.querySelector('.queue-btn')
 const goToLibrary = function () {
   const filmArr = JSON.parse(localStorage.getItem('toWatch'));
-  
+
 }
+console.log(api.getTrends());
