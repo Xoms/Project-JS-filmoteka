5ec38c1c3bfd3458f6fd63109c492404e9bb6281
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-10 12:03:26 +0200

    подключила footer через js, исправила размер svg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import main from './js/mainController';
 import * as basicLightbox from 'basiclightbox';
 import './js/search';
+import './js/footer';
 import 'basicLightbox/dist/basicLightbox.min.css';
 import './sass/main.scss';
 
