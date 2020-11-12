c437c0bb4208fe4a1015de17ecd2aec6afe6c8fe
VINK92 <66158210+VINK92@users.noreply.github.com>
2020-11-12 20:51:29 +0200

    fix telega

--- src/js/telega.js
+++ src/js/telega.js
@@ -1,16 +1,15 @@
 const shareMovie = function() {
   const q = JSON.parse(localStorage.getItem('currentFilm'))
-  console.log(q);
   const title = q.title;
   const voteAverage = q.voteAverage;
   const poster = q.poster;
   const genres = q.genres;
-  const url = 'https://vink92.github.io/goit-js-hw-13';
+  const url = 'https://gal4enock.github.io/Project-JS-filmoteka/';
   document.querySelector('.telega-link').setAttribute('href', 
   `https://telegram.me/share/url?text=${poster}      
   Смотри какой фильм : "${title}" ===>>> 
   Жанр: ${genres} ===>>> 
   Оценка: ${voteAverage} ===>>> 
-  Больше читай тут ===>>> ${url}&url=https://vink92.github.io/goit-js-hw-13`);
+  Больше читай тут ===>>> ${url}&url=${url}`);
 }
  export default shareMovie;
